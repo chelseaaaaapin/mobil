1de3a4510b8eae2f99daddae40d81bdb64eba8ae
chelseaaaaapin <chelseariyantashila06@gmail.com>
2024-12-26 07:51:00 +0700

    Add files via upload

---
+++ mobil.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Mobil;
+
+/**
+ *
+ * @author Chealsea
+ */
+public class mobil {
+    String warna;
+    int tahunProduksi;
+}
---
+++ mobilBeraksi.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Mobil;
+
+/**
+ *
+ * @author Chealsea
+ */
+public class mobilBeraksi {
+    public static void main(String[] args) {
+        mobil mobilChelsea = new mobil();
+        
+        mobilChelsea.warna = "unguu";
+        mobilChelsea.tahunProduksi = 2006;
+        
+        System.out.println("warna : " + mobilChelsea.warna);
+        System.out.println("tahun produksi : " + mobilChelsea.tahunProduksi);
+    }
+    
+}
